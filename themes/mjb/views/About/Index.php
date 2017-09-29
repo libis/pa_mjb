34d75cc0c8399bcb1ec554784a59f90e1caa3870
naeemmuhammad <naeem.muhammad@libis.kuleuven.be>
2017-09-29 16:18:40 +0200

    about page text

--- themes/mjb/views/About/Index.php
+++ themes/mjb/views/About/Index.php
@@ -3,9 +3,24 @@
 ?>
 <H1><?php print _t("About"); ?></H1>
 <div class="row">
-	<div class="col-sm-8">
-		<h6>Contact The Archives</h6>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec ligula erat. Pellentesque nibh leo, pharetra et posuere vel, accumsan vitae sapien. Phasellus a tortor id felis scelerisque blandit. Curabitur a tristique tortor. Morbi non tortor eget dui blandit laoreet. Quisque lacus quam, auctor sit amet volutpat dictum, scelerisque sit amet neque. Vivamus non massa finibus, ultrices nunc vel, scelerisque dui. Aliquam commodo, quam eget fringilla finibus, enim diam sodales ligula, sollicitudin faucibus ligula lorem vitae arcu. Sed efficitur nisi sit amet lobortis malesuada. Ut quis imperdiet elit. Mauris blandit suscipit leo, non tristique est ultrices eu.</p>
+	<div class="col-sm-8" style="text-align: justify">
+        <p>
+            The Jewish Museum of Belgium has decided to place its collection online. This will enable the public to find basic information about the items in our collections. Our inventory is being completed, and the images will be added in due course. Eventually, the entire museum collection will be available, providing you with an overview of the richness of Jewish heritage in Belgium and elsewhere.
+        </p>
+        <h6><b>Generalities</b></h6>
+        <p>
+            The collections of the Jewish Museum of Belgium mainly cover Belgian Judaism from the 18th century onwards. The religious objects, through the diversity of their craftsmanship, style and provenance, recount the history of the Jews in the diaspora and that of various Jewish migrations that converged on Belgium. Conscious of the difficulty of defining a Jewish art, or even a Jewish artist, particularly from the 19th century on, the Jewish Museum of Belgium has not used unduly restrictive criteria in compiling its artistic collections. They contain the works of artists such as: El Lissitsky, Salomon Yudovin, Issashar Ryback, Idel Ianchelevici, Charles Samuel, Kurt Peiser and Felix Nussbaum. The collections also include a significant range of precious objects, textiles and documents that give an account of the social, economic and political life of the different Jewish communities and personalities in Belgium.
+        </p>
+        <h6><b>Improving the information</b></h6>
+        <p>
+            The museum is trying to improve the information that accompanies each item. We invite the public and researchers to help us correct and complete the information. All suggestions should be sent to the email address below.
+        </p>
+        <h6><b>Reproduction</b></h6>
+        <p>
+            As the reproduction of an image requires acknowledgement of its copyright, we invite you to contact the museum.
+        </p>
+        <br>
+        <p><b>Contact</b>: Zahava Seewald, curator <i>z.seewald@mjb-jmb.org</i></p>
 	</div>
 	<div class="col-sm-3 col-sm-offset-1">
 		<h6>&nbsp;</h6><address>Musée Juif de Belgique<br>			rue des Minimes 21<br>			1000 Bruxelles</address>
