1cb4511372344aed821b31be7d1d8713b92829a7
naeemmuhammad <naeem.muhammad@libis.kuleuven.be>
2018-01-11 11:50:34 +0100

    new search fields

--- themes/mjb/views/Search/ca_objects_advanced_search_objects_html.php
+++ themes/mjb/views/Search/ca_objects_advanced_search_objects_html.php
@@ -8,18 +8,48 @@
 ?>
 
 {{{form}}}
-	
-	<div class='advancedContainer'>
-        <div class="advancedSearchField">
-            Free Text Search:<br/>
-            {{{_fulltext%width=200px&height=25px}}}
+	<div class="row">	
+		<div class='advancedContainer'>
+			<div class="advancedSearchField">
+			Free Text Search:<br/>
+			{{{_fulltext%width=220px&height=25px}}}
+			</div>
+			<div class="advancedSearchField">
+				Title:<br/>
+				{{{ca_objects.preferred_labels.name%width=220px}}}
+			</div>
+			<div class="advancedSearchField">
+			Auteur:<br/>
+			{{{ca_objects.auteur.auteurNom%width=220px&height=28px}}}
+			</div>
+		</div>	
+	</div>
+	<br>
+    <div class="row">
+        <div class='advancedContainer'>
+            <div class="advancedSearchField">
+                Date de Début:<br/>
+                {{{ca_objects.date.dateDebut%width=220px&height=25px}}}
+            </div>
+            <div class="advancedSearchField">
+                Date de Fin:<br/>
+                {{{ca_objects.date.datefin%width=220px&height=25px}}}
+            </div>
+            <div class="advancedSearchField">
+                Provenance Géographique:<br/>
+                {{{ca_objects.provenanceGeographique%width=220px&height=25px}}}
+            </div>
         </div>
-		<div class="advancedSearchField">
-			Title:<br/>
-			{{{ca_objects.preferred_labels.name%width=220px}}}
-		</div>
-	</div>	
-	
+    </div>
+    <br>
+    <div class="row">
+        <div class='advancedContainer'>
+            <div class="advancedSearchField">
+                Dénomination Contrôlée:<br/>
+                {{{ca_objects.denominationControleeMusee%width=220px&height=28px}}}
+            </div>
+        </div>
+    </div>
 	<br style="clear: both;"/>
 	
 	<div style="float: right; margin-left: 20px;">{{{reset%label=Reset}}}</div>
