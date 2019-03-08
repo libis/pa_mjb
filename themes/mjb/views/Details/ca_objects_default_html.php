87589bdd5e33323379fa414074ba0409c00a4b75
naeemmuhammad <naeem.muhammad@libis.kuleuven.be>
2019-03-08 12:27:37 +0100

    type specific detail page

--- themes/mjb/views/Details/ca_objects_default_html.php
+++ themes/mjb/views/Details/ca_objects_default_html.php
@@ -30,6 +30,9 @@
 	$va_comments = 			$this->getVar("comments");
 	$vn_comments_enabled = 	$this->getVar("commentsEnabled");
 	$vn_share_enabled = 	$this->getVar("shareEnabled");
+    $va_browse_info = $this->getVar("browseInfo");
+
+    $obj_type = $t_object->getTypeCode(); //objetMusee,biblio,photo
 
 ?>
 <div class="row">
@@ -46,7 +49,7 @@
 			<div class='col-sm-6 col-md-6 col-lg-5 col-lg-offset-1'>
 				{{{representationViewer}}}
 				
-				
+
 				<div id="detailAnnotations"></div>
 				
 				<?php print caObjectRepresentationThumbnails($this->request, $this->getVar("representation_id"), $t_object, array("returnAs" => "bsCols", "linkTo" => "carousel", "bsColClasses" => "smallpadding col-sm-3 col-md-3 col-xs-4")); ?>
@@ -69,7 +72,7 @@
 				}				
 ?>
 			</div><!-- end col -->
-			
+			<?php if($obj_type === "objetMusee") { ?>
 			<div class='col-sm-6 col-md-6 col-lg-5'>
 				<H4>{{{<unit relativeTo="ca_collections" delimiter="<br/>"><l>^ca_collections.preferred_labels.name</l></unit><ifcount min="1" code="ca_collections"> ➔ </ifcount>}}}{{{ca_objects.preferred_labels.name}}}</H4>
 				<H6>{{{<unit>^ca_objects.type_id</unit>}}}</H6>
@@ -112,12 +115,87 @@
 
                 {{{<ifdef code="ca_objects.materiaux" min="1"><H6>Matériaux</H6></ifdef>}}}
                 {{{<unit relativeTo="ca_objects.materiaux" delimiter="; ">^ca_objects.materiaux</unit>}}}
-
-                {{{<ifdef code="ca_objects.techniques" min="1"><H6>Techniques</H6></ifdef>}}}
-                {{{<unit relativeTo="ca_objects.techniques" delimiter="; ">^ca_objects.techniques</unit>}}}				
 				
+                {{{<ifdef code="ca_objects.techniques" min="1"><H6>Techniques</H6></ifdef>}}}
+                {{{<unit relativeTo="ca_objects.techniques" delimiter="; ">^ca_objects.techniques</unit>}}}
+
 				<hr></hr>
 			</div><!-- end col -->
+                <?php } ?>
+                <?php if($obj_type === "biblio") { ?>
+                <div class='col-sm-6 col-md-6 col-lg-5'>
+                    <H4>{{{<unit relativeTo="ca_collections" delimiter="<br/>"><l>^ca_collections.preferred_labels.name</l></unit><ifcount min="1" code="ca_collections"> ➔ </ifcount>}}}{{{ca_objects.preferred_labels.name}}}</H4>
+                    <H6>{{{<unit>^ca_objects.type_id</unit>}}}</H6>
+                    <HR>
+                    {{{<ifdef code="ca_objects.idno"><H6>N° Inventaire:</H6>^ca_objects.idno<br/></ifdef>}}}
+                    {{{<ifdef code="ca_objects.codeBiblio" min="1"><H6>Code</H6></ifdef>}}}
+                    {{{<unit relativeTo="ca_objects" delimiter="<br/>">
+                        <ifdef code="ca_objects.codeBiblio.sujet">^ca_objects.codeBiblio.sujet</ifdef>
+                        <ifdef code="ca_objects.codeBiblio.nom1">- ^ca_objects.codeBiblio.nom1</ifdef>
+                        <ifdef code="ca_objects.codeBiblio.titre1">- ^ca_objects.codeBiblio.titre1</ifdef>
+                    </unit>}}}
+                    {{{<ifcount code="ca_objects.denominationControleeBiblio" min="1"><H6>Dénomination contrôlée</H6></ifcount>}}}
+                    {{{<unit relativeTo="ca_objects.denominationControleeBiblio" delimiter="<br/>">^ca_objects.denominationControleeBiblio</unit>}}}
+                    {{{<ifdef code="ca_objects.preferred_labels.name"><H6>Titre:</H6>^ca_objects.preferred_labels.name<br/></ifdef>}}}
+                    {{{<ifdef code="ca_objects.auteurBiblio" min="1"><H6>Auteur</H6></ifdef>}}}
+                    {{{<unit relativeTo="ca_objects" delimiter="<br/>">
+                        <b>^ca_objects.auteurBiblio.auteurNomBiblio</b> <ifdef code="ca_objects.auteurBiblio.auteurNomBiblio">(^ca_objects.auteurBiblio.fonctionBiblio)</ifdef>
+                    </unit>}}}
+                    {{{<ifcount code="ca_objects.dateEdition" min="1"><H6>Date d'édition</H6></ifcount>}}}
+                    {{{<unit relativeTo="ca_objects.dateEdition" delimiter="<br/>">^ca_objects.dateEdition</unit>}}}
+                    {{{<ifcount code="ca_objects.lieuEdition" min="1"><H6>Lieu d'Edition</H6></ifcount>}}}
+                    {{{<unit relativeTo="ca_objects.lieuEdition" delimiter="<br/>">^ca_objects.lieuEdition</unit>}}}
+                    {{{<ifcount code="ca_objects.langues" min="1"><H6>Langue</H6></ifcount>}}}
+                    {{{<unit relativeTo="ca_objects.langues" delimiter="<br/>">^ca_objects.langues</unit>}}}
+                    {{{<ifdef code="ca_objects.dimensions" min="1"><H6>Dimensions?????</H6></ifdef>}}}
+
+                    {{{<unit relativeTo="ca_objects" delimiter="<br/>">
+                        <ifdef code="ca_objects.dimensions.dimensions_hauteur">Hauteur:^ca_objects.dimensions.dimensions_hauteur;</ifdef>
+                        <ifdef code="ca_objects.dimensions.dimensions_largeur"> Largeur:^ca_objects.dimensions.dimensions_largeur;</ifdef>
+                        <ifdef code="ca_objects.dimensions.dimensions_epaisseur"> Epaisseur:^ca_objects.dimensions.dimensions_epaisseur;</ifdef>
+                        <ifdef code="ca_objects.dimensions.dimensions_diametre"> Diametre:^ca_objects.dimensions.dimensions_diametre</ifdef>
+                        <ifdef code="ca_objects.dimensions.dimensionsPartie">(^ca_objects.dimensions.dimensionsPartie)</ifdef>
+                        <ifdef code="ca_objects.dimensions.dimensionRemarque">(^ca_objects.dimensions.dimensionRemarque)</ifdef>
+                    </unit>}}}
+                </div><!-- end col -->
+                <?php } ?>
+                <?php if($obj_type === "photo") { ?>
+                    <div class='col-sm-6 col-md-6 col-lg-5'>
+                        <H4>{{{<unit relativeTo="ca_collections" delimiter="<br/>"><l>^ca_collections.preferred_labels.name</l></unit><ifcount min="1" code="ca_collections"> ➔ </ifcount>}}}{{{ca_objects.preferred_labels.name}}}</H4>
+                        <H6>{{{<unit>^ca_objects.type_id</unit>}}}</H6>
+                        <HR>
+                        {{{<ifdef code="ca_objects.idno"><H6>N° Inventaire:</H6>^ca_objects.idno<br/></ifdef>}}}
+                        {{{<ifdef code="ca_objects.preferred_labels.name"><H6>Titre:</H6>^ca_objects.preferred_labels.name<br/></ifdef>}}}
+
+                        {{{<ifcount code="ca_objects.descriptionRepresentation" min="1"><H6>Description de la Représentation</H6></ifcount>}}}
+                        {{{<unit relativeTo="ca_objects.descriptionRepresentation" delimiter="<br/>">^ca_objects.descriptionRepresentation</unit>}}}
+                        {{{<ifcount code="ca_objects.denominationControleePhoto" min="1"><H6>Dénomination contrôlée</H6></ifcount>}}}
+                        {{{<unit relativeTo="ca_objects.denominationControleePhoto" delimiter="<br/>">^ca_objects.denominationControleePhoto</unit>}}}
+                        {{{<ifcount code="ca_objects.descriptionContenu" min="1"><H6>Description du contenu</H6></ifcount>}}}
+                        {{{<unit relativeTo="ca_objects.descriptionContenu" delimiter="<br/>">^ca_objects.descriptionContenu</unit>}}}
+                        {{{<ifdef code="ca_objects.auteur" min="1"><H6>Auteur</H6></ifdef>}}}
+                        {{{<unit relativeTo="ca_objects" delimiter="<br/>"><b>^ca_objects.auteur.auteurNom</b>
+                            <ifdef code="ca_objects.auteur.fonction">(^ca_objects.auteur.fonction)</ifdef>
+                        </unit>}}}
+                        {{{<ifcount code="ca_objects.dateExacte" min="1"><H6>Date Exacte</H6></ifcount>}}}
+                        {{{<unit relativeTo="ca_objects.dateExacte" delimiter="<br/>">^ca_objects.dateExacte</unit>}}}
+                        {{{<ifcount code="ca_objects.provenanceGeographique" min="1"><H6>Provenance Géographique</H6></ifcount>}}}
+                        {{{<unit relativeTo="ca_objects.provenanceGeographique" delimiter="<br/>">^ca_objects.provenanceGeographique</unit>}}}
+                        {{{<ifcount code="ca_objects.techniques" min="1"><H6>Techniques</H6></ifcount>}}}
+                        {{{<unit relativeTo="ca_objects.techniques" delimiter="<br/>">^ca_objects.techniques</unit>}}}
+
+
+                        {{{<ifdef code="ca_objects.dimensions" min="1"><H6>Dimensions</H6></ifdef>}}}
+                        {{{<unit relativeTo="ca_objects" delimiter="<br/>">
+                            <ifdef code="ca_objects.dimensions.dimensions_hauteur">Hauteur:^ca_objects.dimensions.dimensions_hauteur;</ifdef>
+                            <ifdef code="ca_objects.dimensions.dimensions_largeur"> Largeur:^ca_objects.dimensions.dimensions_largeur;</ifdef>
+                            <ifdef code="ca_objects.dimensions.dimensions_epaisseur"> Epaisseur:^ca_objects.dimensions.dimensions_epaisseur;</ifdef>
+                            <ifdef code="ca_objects.dimensions.dimensions_diametre"> Diametre:^ca_objects.dimensions.dimensions_diametre</ifdef>
+                            <ifdef code="ca_objects.dimensions.dimensionsPartie">(^ca_objects.dimensions.dimensionsPartie)</ifdef>
+                            <ifdef code="ca_objects.dimensions.dimensionRemarque">(^ca_objects.dimensions.dimensionRemarque)</ifdef>
+                        </unit>}}}
+                    </div><!-- end col -->
+                <?php } ?>
 		</div><!-- end row --></div><!-- end container -->
 	</div><!-- end col -->
 	<div class='navLeftRight col-xs-1 col-sm-1 col-md-1 col-lg-1'>
