3129ab59308f12b322be78ff9ece89f62827d016
naeemmuhammad <naeem.muhammad@libis.kuleuven.be>
2019-03-12 14:36:17 +0100

    all collections search form

--- themes/mjb/views/Search/ca_objects_advanced_search_objects_html.php
+++ themes/mjb/views/Search/ca_objects_advanced_search_objects_html.php
@@ -1,60 +1,35 @@
 <div class="container">
 	<div class="row">
 		<div class="col-sm-8 ">
-			<h1>Objects Advanced Search</h1>
+			<h1>All Collections Search</h1>
 
-<?php			
+<?php
 	print "<p>Enter your search terms in the fields below.</p>";
 ?>
 
 {{{form}}}
 	<div class="row">	
-		<div class='advancedContainer'>
-			<div class="advancedSearchField">
-			Free Text Search:<br/>
-			{{{_fulltext%width=220px&height=25px}}}
-			</div>
-			<div class="advancedSearchField">
-				Titre:<br/>
-				{{{ca_objects.preferred_labels.name%width=220px}}}
-			</div>
-			<div class="advancedSearchField">
-			Auteur:<br/>
-			{{{ca_objects.auteur.auteurNom%width=220px&height=28px}}}
-			</div>
-		</div>	
+	<div class='advancedContainer'>
+         <div class="advancedSearchField">
+            Free Text Search:<br/>
+            {{{_fulltext%width=220px&height=25px}}}
+         </div>
+         <div class="advancedSearchField">
+                Auteur:<br/>
+                {{{ca_objects.auteur.auteurNom%width=220px&height=28px}}}
+          </div>
+	</div>	
 	</div>
-	<br>
+<br>
     <div class="row">
         <div class='advancedContainer'>
             <div class="advancedSearchField">
-                Date de Début:<br/>
-                {{{ca_objects.date.dateDebut%width=220px&height=25px}}}
-            </div>
-            <div class="advancedSearchField">
-                Date de Fin:<br/>
-                {{{ca_objects.date.datefin%width=220px&height=25px}}}
+                Date:<br/>
+                {{{_alldates%width=220px&height=25px}}}
             </div>
             <div class="advancedSearchField">
                 Provenance Géographique:<br/>
                 {{{ca_objects.provenanceGeographique%width=220px&height=25px}}}
-            </div>
-        </div>
-    </div>
-    <br>
-    <div class="row">
-        <div class='advancedContainer'>
-            <div class="advancedSearchField">
-                Dénomination Contrôlée:<br/>
-                {{{ca_objects.denominationControleeMusee%width=220px&height=28px}}}
-            </div>
-            <div class="advancedSearchField">
-                Evéments en rappor:<br/>
-                {{{ca_objects.evenementsRapport%width=220px&height=28px}}}
-            </div>
-            <div class="advancedSearchField">
-                Organisation en rapport:<br/>
-                {{{ca_objects.organisationRapport%width=220px&height=28px}}}
             </div>
         </div>
     </div>
