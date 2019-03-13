484b72c6cd66a8310a61815513309b0b7060b266
naeemmuhammad <naeem.muhammad@libis.kuleuven.be>
2019-03-13 11:57:44 +0100

    photography advanced search

---
+++ themes/mjb/views/Search/ca_objects_advanced_search_photo_objects_html.php
@@ -0,0 +1,44 @@
+<div class="container">
+	<div class="row">
+		<div class="col-sm-8 ">
+			<h1>Photo Collection Search</h1>
+
+<?php
+	print "<p>Enter your search terms in the fields below.</p>";
+?>
+
+{{{form}}}
+	<div class="row">	
+        <div class='advancedContainer'>
+             <div class="advancedSearchField">
+                Free Text Search:<br/>
+                {{{_fulltext%restrictToTypes=Photo&width=220px&height=25px}}}
+             </div>
+             <div class="advancedSearchField">
+                    Auteur:<br/>
+                    {{{ca_objects.auteur.auteurNom%restrictToTypes=Photo&width=220px&height=28px}}}
+              </div>
+        </div>
+	</div>
+<br>
+    <div class="row">
+        <div class='advancedContainer'>
+            <div class="advancedSearchField">
+                Provenance Géographique:<br/>
+                {{{ca_objects.provenanceGeographique%restrictToTypes=Photo&width=220px&height=28px}}}
+            </div>
+            <div class="advancedSearchField">
+                Description de la Représentation:<br/>
+                {{{ca_objects.descriptionRepresentation%restrictToTypes=Photo&width=220px&height=28px}}}
+            </div>
+        </div>
+    </div>
+	<br style="clear: both;"/>
+	
+	<div style="float: right; margin-left: 20px;">{{{reset%label=Reset}}}</div>
+	<div style="float: right;">{{{submit%label=Search}}}</div>
+{{{/form}}}
+
+		</div>
+	</div><!-- end row -->
+</div><!-- end container -->
